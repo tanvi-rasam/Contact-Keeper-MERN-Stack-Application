0ca74dbb2c7072e5c4d3eb54cda396fb8f84b779
tanvi-rasam <trasam@uncc.edu>
2020-06-04 04:20:23 -0400

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const app=express()
+
+
+
+app.get('/', (req,res)=>
+    res.send("Helo")
+);
+
+app.use('/api/users',require('./routes/users'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/contacts',require('./routes/contacts'));
+
+PORT=process.env.PORT|| 5000
+
+app.listen(PORT,()=>console.log("Server Started"));
